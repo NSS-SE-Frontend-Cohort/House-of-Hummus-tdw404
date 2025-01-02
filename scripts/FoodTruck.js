c3d54f889a7249d3cca83fc0516a833c0fc7b374
Tom Walsh <walsth@gmail.com>
2025-01-02 16:03:40 -0600

    Added basic formatting for order categories

--- scripts/FoodTruck.js
+++ scripts/FoodTruck.js
@@ -9,6 +9,18 @@ export const FoodTruck = () => {
             <h1 class="title">Laura Kathryn's House of Hummus</h1>
         </header>
 
+        <div class="choices">
+            <article class ="options">
+                <h2>Entrees</h2>
+            </article>
+            <article class ="options">
+                <h2>Vegetables</h2>
+            </article>
+            <article class ="options">
+                <h2>Sides</h2>
+            </article>
+        </div>
+        
         <article>
             <button id="purchase">Purchase Combo</button>
         </article>
